8ccba3d56f7d0e13b23685a0c0b1cee2be313c69
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2025-09-03 10:54:10 +0600

    Header Part done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ const Header = () => {
     <div className='p-5 md:px-12 lg:px-24 bg-gray-800 text-white font-bold text-2xl'>
       <div className='flex justify-center items-center'>
         <Image src={assets.logo} alt='logo' width={50} height={50} className='mr-3'/>
+        
       </div>
       
     </div>
